fed7defeb4fed847c528be5f131e427d8d75f621
Atif-Aawan <110194423+Atif-Aawan@users.noreply.github.com>
2022-08-25 21:36:04 +0500

    Template Setting

--- application/controllers/Web_Home.php
+++ application/controllers/Web_Home.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Web_Home extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,8 +18,16 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
+
+	public function __construct() 
 	{
-		$this->load->view('welcome_message');
+        parent::__construct();
+        error_reporting(0);
+    }
+
+	public function index()
+	{ 
+		$data['web_main_content'] = 'web/index';
+        $this->load->view('includes/web_template', $data);
 	}
 }
---
+++ application/views/includes/web_template.php
@@ -0,0 +1,5 @@
+<?php $this->load->view('includes/web_header'); ?>
+
+<?php $this->load->view($web_main_content); ?>
+
+<?php $this->load->view('includes/web_footer'); ?>
